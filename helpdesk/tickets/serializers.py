4ffef240097df724031a882368bec8ea45282db6
Brianojunga <brianojunga3@gmail.com>
2025-12-18 23:55:32 +0300

    Remove serializers for profiles and add atomicity for company to update the company owner

--- helpdesk/tickets/serializers.py
+++ helpdesk/tickets/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import Ticket, Company
-from django.contrib.auth.models import User
+from django.db import transaction
+from django.conf import  settings
+
+User = settings.AUTH_USER_MODEL
 
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,46 +17,30 @@ class TicketSerializer(serializers.ModelSerializer):
             validated_data['user'] = request.user
         validated_data['company'] = self.context['company']
         return super().create(validated_data)
-    
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-
-    def validate(self, data):
-        email = data.get('email', None)
-        username = data.get('username')
-        if not email:
-            raise serializers.ValidationError("Email is required.")
-        
-        if User.objects.filter(email=email).exists():
-            raise serializers.ValidationError("Email is already in use.")
-        
-        if User.objects.filter(username=username).exists():
-            raise serializers.ValidationError("Username is already taken.")
-
-
-    def create(self, validated_data):
-        user = User(
-            username=validated_data['username'],
-            email=validated_data['email']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
-    
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
-        read_only_fields = ['id']
 
 
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
-        fields = ['id', 'name', 'slug']
+        fields = ['id', 'name', 'slug', 'user']
+        read_only_fields = ['id', 'user']
+
+    @transaction.atomic   
+    def create(self, validated_data):
+        request = self.context.get('request')
+        user = request.user
         
+        if not user.is_authenticated:
+            raise serializers.ValidationError('Authentication is required')
+        
+        company = Company.objects.create(
+            owner=user,
+            **validated_data
+        )
+
+        user.company = company
+        user.role='owner'
+        user.save(update_fields=['company', 'role'])
+
+        return company
     
