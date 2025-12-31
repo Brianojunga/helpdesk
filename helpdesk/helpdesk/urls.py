00a53ebd618308ad53a92918851667b5a8511b00
Brianojunga <brianojunga3@gmail.com>
2025-12-31 20:09:46 +0300

    Add swagger ui for documentation

--- helpdesk/helpdesk/urls.py
+++ helpdesk/helpdesk/urls.py
@@ -15,10 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+)
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Helpdesk API",
+      default_version='v1',
+      description="Helpdesk API",
+   ),
+   public=True,
+   permission_classes=[AllowAny]
 )
 
 urlpatterns = [
@@ -26,4 +39,8 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('tickets.urls')),
+    path('api/', include('accounts.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
