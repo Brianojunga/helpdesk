b36a402b731c676ceb1867d95e283e3c2c9c3570
Brianojunga <brianojunga3@gmail.com>
2026-01-03 20:53:05 +0300

    Update the paths for custom user to prevent circular dependency

--- helpdesk/accounts/models.py
+++ helpdesk/accounts/models.py
@@ -1,10 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
-class staff(models.Model):
+class CustomUser(AbstractUser):
     STAFF_ROLES = [
         ('admin', 'Admin'),
         ('agent', 'Agent'),
-        ('manager', 'Manager')
+        ('owner', 'Owner'),
+        ('customer', 'Customer')
     ]
+    company = models.ForeignKey(
+        'tickets.Company', 
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='company_staff'
+        )
+    role = models.CharField(max_length=20, choices=STAFF_ROLES, default='customer')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
